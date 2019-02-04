0dd7823f6b58d8a6d2b109900ce21f99525172b3
Michal <michal.drabik@neoncu.be>
2019-02-04 13:10:16 +0700

    Basic project setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header/header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style lang="scss">
+html, body {
+  position: relative;
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="header__content">
+      <div class="header__item-image">
+        <img alt="Showly screenshot" src="@/assets/screen1.png">
+      </div>
+      <div class="header__item-description">
+        <img
+          class="header__item-description-icon"
+          alt="Showly icon"
+          src="@/assets/icon.png">
+        <div class="header__item-description-title">
+          Follow the best TV shows on the air!
+        </div>
+        <div class="header__item-description-subtitle">
+          Showly is easy to use app designed with the latest Android guidelines in mind.
+        </div>
+        <a :href="playStoreLink">
+          <img
+            class="header__item-description-googleplay"
+            alt="Showly icon"
+            src="@/assets/google_play.png">
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      playStoreLink: 'https://play.google.com/store/apps/details?id=com.michaldrabik.seriestoday&hl=en'
+    }
+  }
+}
+</script>
+
+<style src="./header.scss" lang="scss" scoped></style>
